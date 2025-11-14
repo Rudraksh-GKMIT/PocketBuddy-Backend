9b8899b26450eeb1e11819b033db0198cbec198d
rudraksh-GKMIT <rudraksh@gkmit.co>
2025-11-14 10:25:11 +0530

    feat:  add transaction schema

--- app/schema/schema.py
+++ app/schema/schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel , EmailStr
 
 class Rolebase(BaseModel):
     name: str
@@ -8,15 +8,52 @@ class RoleCreate(Rolebase):
  
 class UserCreate(BaseModel):
     name : str
-    email : constr(strip_whitespace=True, to_lower=True)
-    password : constr(min_length=4, max_length=72)
+    email : EmailStr
+    password : str
     family_name : str
 
 class UserLogin(BaseModel):
-    email : constr(strip_whitespace=True, to_lower=True)
-    password : constr(min_length=4, max_length=72)
+    email : EmailStr
+    password : str
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+class MemberCreate(BaseModel):
+    name : str
+    email : EmailStr
+    password : str
+
+class MemberResponse(BaseModel):
+    id: str
+    name : str
+    email : EmailStr
+
+    class Config:
+        from_attributes = True
+
+class MemberUpdate(BaseModel):
+    name: str | None = None
+    email: EmailStr | None = None
+    password: str | None = None
+    role: str | None = None
+
+class TransactionCreate(BaseModel):
+    type: str
+    amount: float
+    description: str | None = None
+
+class TransactionUpdate(BaseModel):
+    type: str |  None = None
+    amount: float |  None = None
+    description: str |  None = None
+
+class TransactionResponse(BaseModel):
+    id: int
+    type: str
+    amount: float
+    description: str | None
     
+    class Config:
+        from_attributes = True
