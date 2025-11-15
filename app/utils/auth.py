cad5d77998027f43db5a6c1c19c40ad893deb6fc
rudraksh-GKMIT <rudraksh@gkmit.co>
2025-11-15 19:36:48 +0530

    fix : add verifying password function to check for login

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -22,6 +22,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
