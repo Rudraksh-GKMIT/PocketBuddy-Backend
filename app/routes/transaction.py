774d2a0e0c27e4d46e16e86dc186f374cc3e69ed
rudraksh-GKMIT <rudraksh@gkmit.co>
2025-11-16 20:24:08 +0530

    chore : add all the require imports for the file

--- app/routes/transaction.py
+++ app/routes/transaction.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
 from app.db.database import get_db
-from app.db.seeding import seed_roles
 from app.constants import ADMIN
+from app.schema.schema import TransactionResponse, TransactionCreate, TransactionUpdate
+from app.model.transactions import Transaction
+from app.model.users import User
+from app.utils.auth import get_current_user
+from uuid import UUID
 
-router = APIRouter(prefix="/api/transaction",tags= ["Transaction"])
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+router = APIRouter(prefix="/api/transaction", tags=["Transaction"])
+
