e598048549af38598dfef8dce229ea3175d5da69
rudraksh-GKMIT <rudraksh@gkmit.co>
2025-11-16 23:32:37 +0530

    feat : add api to see all of the user transactions

--- app/routes/transaction.py
+++ app/routes/transaction.py
@@ -10,3 +10,12 @@ from uuid import UUID
 
 router = APIRouter(prefix="/api/transaction", tags=["Transaction"])
 
+
+@router.get("/my", response_model=list[TransactionResponse])
+def get_my_transactions(
+    db: Session = Depends(get_db), current=Depends(get_current_user)
+):
+    user, roles = current
+    transaction = db.query(Transaction).filter(Transaction.user_id == user.id).all()
+    return transaction
+
