dd98eb69ed2eef578ecdcdcf35d5cc8ed62fbbab
rudraksh-GKMIT <rudraksh@gkmit.co>
2025-11-17 21:46:02 +0530

    feat : add family summary api

--- app/routes/summary.py
+++ app/routes/summary.py
@@ -22,3 +22,24 @@ def get_my_transaction(
         .scalar()
     )
     return {"user_id": user.id, "total_expense": total}
+
+
+@router.get("/family")
+def get_family_transaction(
+    db: Session = Depends(get_db), current=Depends(get_current_user)
+):
+    user, roles = current
+    if "admin" not in roles:
+        raise HTTPException(status_code=403, detail="Only admin can view this")
+
+    family_user = db.query(User.id).filter(
+        User.family_id == user.family_id, User.deleted_at.is_(None)
+    )
+    total = (
+        db.query(func.sum(Transaction.amount))
+        .filter(Transaction.user_id.in_(family_user))
+        .scalar()
+    )
+
+    return {"family_id": user.family_id, "total_family_expense": total}
+
